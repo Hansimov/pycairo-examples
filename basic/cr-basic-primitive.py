3d8408d662d4f774b9e6f61565506054808555da
Hansimov <591172499@qq.com>
2018-07-19 21:51:10 +0800

    + Add some init files

---
+++ basic/cr-basic-primitive.py
@@ -0,0 +1,8 @@
+import sys
+sys.path.append('../library/')
+
+# import header
+from header import *
+
+# print(header.math.pi)
+print(math.pi)
---
+++ library/shape.py
@@ -0,0 +1,15 @@
+def circle():
+    pass
+
+# def rectangle():
+#     pass
+
+def square():
+    pass
+
+def triangle():
+    pass
+
+def polygon():
+    pass
+
